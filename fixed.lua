ed48c94503d80057d7cb76ba99e7d32dd5f4c9ec
yk <103144571+d6monic@users.noreply.github.com>
2022-12-14 02:12:00 -0500

    Update fixed.lua

--- fixed.lua
+++ fixed.lua
@@ -1,21 +1,16 @@
--- // Dependencies
-_G.PRED = 0.025
-local Aiming = loadstring(game:HttpGet("https://gist.githubusercontent.com/s17tzz/b3360969505c1f0f18950c1e1feb474f/raw/3e9e27e5856ef41efe14139ee5ef4d0abd36bda1/xp2jgvJNRxNKkM.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaiCuh/locks/main/d870997929af2a1483180013f688c771"))()
 Aiming.TeamCheck(false)
-Aiming.ShowFOV = true
-Aiming.FOV = 25
--- // Services
+
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
--- // Vars
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local CurrentCamera = Workspace.CurrentCamera
 
---// Spoof the Current Anticheat
+--// Spoof the Current Da Hood Modded Anti Cheat
 local function ACThing()
     for I, V in pairs(getgc(true)) do
         if typeof(V) == "function" then
@@ -38,33 +33,28 @@ local function ACThing()
 end
 ACThing()
 game.Players.LocalPlayer.CharacterAdded:connect(ACThing)
-
+        
 local DaHoodSettings = {
     SilentAim = true,
     AimLock = false,
-    Prediction = 0.025,
-    AimLockKeybind = Enum.KeyCode.E
+    Prediction = 0.14,
+    AimLockKeybind = Enum.KeyCode.T,
 }
 getgenv().DaHoodSettings = DaHoodSettings
 
--- // Overwrite to account downed
 function Aiming.Check()
-    -- // Check A
     if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
         return false
     end
 
-    -- // Check if downed
     local Character = Aiming.Character(Aiming.Selected)
     local KOd = Character:WaitForChild"K.O".Value
     local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
 
-    -- // Check B
     if (KOd or Grabbed) then
         return false
     end
 
-    -- //
     return true
 end
 
@@ -87,44 +77,13 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
     return oldIndex(self, Index)
 end)
 
-local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()
 
-mouse.KeyDown:Connect(function(key)
-    if key == "v" then
-        if Aiming.Enabled == false then
-        Aiming.Enabled = true
-        else
-        Aiming.Enabled = false
-        end
+RunService:BindToRenderStep("AimLock", 0, function()
+    if (DaHoodSettings.AimLock and Aiming.Check() and UserInputService:IsKeyDown(DaHoodSettings.AimLockKeybind)) then
+        local SelectedPart = Aiming.SelectedPart
+
+        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+
+        CurrentCamera.CFrame = CFrame.lookAt(CurrentCamera.CFrame.Position, Hit.Position)
     end
-end)
-
-
-RunService.RenderStepped:Connect(function()
-
-    local ping = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()
-    local Value = tostring(ping)
-    local pingValue = Value:split(" ")
-    local PingNumber = pingValue[1]
-    
-    DaHoodSettings.Prediction = PingNumber / 1000 + _G.PRED
-    
-                    if Aiming.Character.Humanoid.Jump == true and AimlockTarget.Character.Humanoid.FloorMaterial == Enum.Material.Air then
-                    Aiming.TargetPart = "RightFoot"
-                else
-                    Aiming.Character:WaitForChild("Humanoid").StateChanged:Connect(function(new)
-                    
-                    if new == Enum.HumanoidStateType.Freefall then
-                    Aiming.TargetPart = "RightFoot"
-                    else
-                    
-                    Aiming.TargetPart = Aiming.SelectedPart
-                    
-                    end
-                    
-                    end)
-                    
-                end
-
 end)
