6bfc5ac7ba87545f12408aa494582a02a89eb74f
yk <103144571+d6monic@users.noreply.github.com>
2022-12-15 16:26:45 -0500

    Update fixed.lua

--- fixed.lua
+++ fixed.lua
@@ -88,45 +88,6 @@ RunService:BindToRenderStep("AimLock", 0, function()
     end
 end)
 
-getgenv().VelocityChanger = true
-getgenv().Velocity = Vector3.new(200,0,200)
-
-
---// main scapt 
-local Players     = game:GetService("Players")
-local RunService  = game:GetService("RunService")
-
-local LocalPlayer = Players.LocalPlayer
-local Character   = LocalPlayer.Character
-local RootPart    = Character:FindFirstChild("HumanoidRootPart")
-
-local Heartbeat, RStepped, Stepped = RunService.Heartbeat, RunService.RenderStepped, RunService.Stepped
-
-LocalPlayer.CharacterAdded:Connect(function(NewCharacter)
-   Character = NewCharacter
-end)
-
-local RVelocity, YVelocity = nil, 0.1
-
-while true do
-   if VelocityChanger then
-       --// this a dumb check 
-       if (not RootPart) or (not RootPart.Parent) or (not RootPart.Parent.Parent) then
-           warn("weird ahh died")
-           RootPart = Character:FindFirstChild("HumanoidRootPart")
-       else
-           RVelocity = RootPart.Velocity
-   
-           RootPart.Velocity = type(Velocity) == "vector" and Velocity or Velocity(RVelocity)
-       
-           RStepped:wait()
-       
-           RootPart.Velocity = RVelocity
-       end
-   end
-   
-   Heartbeat:wait()
-end
 
 game.Players.LocalPlayer.Chatted:Connect(function(ReV)
 	if ReV == "/e resv2" or "/e rev2" or "/e v2" then
